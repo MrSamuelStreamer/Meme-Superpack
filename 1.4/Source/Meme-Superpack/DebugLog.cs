a9e6291bfbc6e52194977288100e767b9f3ee176
Dexter Lowe <feldoh10@gmail.com>
2023-03-13 01:25:39 +0000

    Initial Commit

---
+++ 1.4/Source/Meme-Superpack/DebugLog.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace MSS.MemeSuperpack
+{
+	static class Log
+	{
+		[Conditional("DEBUG")]
+		public static void Message(string x)
+		{
+			Verse.Log.Message(x);
+		}
+	}
+}
---
+++ 1.4/Source/Meme-Superpack/MemeSuperpackMod.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace MSS.MemeSuperpack
+{
+	public class MemeSuperpackMod : Mod
+	{
+		public static Settings settings;
+
+		public MemeSuperpackMod(ModContentPack content) : base(content)
+		{
+			Log.Message("Hello world from Meme-Superpack");
+
+			// initialize settings
+			settings = GetSettings<Settings>();
+
+#if DEBUG
+			Harmony.DEBUG = true;
+#endif
+
+			Harmony harmony = new Harmony("MrSamuelStreamer.rimworld.Meme-Superpack.main");
+			harmony.PatchAll();
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			base.DoSettingsWindowContents(inRect);
+			settings.DoWindowContents(inRect);
+		}
+
+		public override string SettingsCategory()
+		{
+			return "Meme-Superpack";
+		}
+	}
+}
