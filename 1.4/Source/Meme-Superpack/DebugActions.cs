48318b121955b34b6b83a18519a5cea01edd7e8a
Dexter Lowe <feldoh10@gmail.com>
2023-04-01 05:08:04 +0100

    Added eclipse toggle

--- 1.4/Source/Meme-Superpack/DebugActions.cs
+++ 1.4/Source/Meme-Superpack/DebugActions.cs
@@ -6,10 +6,11 @@ namespace MSS.MemeSuperpack;
 
 public static class DebugActions
 {
-	[DebugAction("Memes", "Reset AllFather - Arcadius", false, false, false, 0, false, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+	[DebugAction("Memes", "Reset AllFather - Arcadius", false, false, false, 0, false,
+		allowedGameStates = AllowedGameStates.PlayingOnMap)]
 	private static void ReApplyYChromosomalAdam()
 	{
-		if (GameComponent_ArcadiusRelationManager.GetArcadius() is not {} arcadius) return;
+		if (GameComponent_ArcadiusRelationManager.GetArcadius() is not { } arcadius) return;
 		foreach (Pawn p in PawnsFinder.All_AliveOrDead.Where(pawn =>
 			         pawn.RaceProps is { } raceProps && raceProps.Humanlike && raceProps.IsFlesh &&
 			         raceProps.intelligence == Intelligence.Humanlike && pawn != arcadius &&
@@ -31,5 +32,24 @@ public static class DebugActions
 	private static void StopGaslighting()
 	{
 		GameComponent_MemeTracker.Instance.EndGaslighting();
+	}
+
+	[DebugAction("Memes", "Toggle Eclipse", false, false, false, 0, false,
+		allowedGameStates = AllowedGameStates.PlayingOnMap)]
+	private static void ToggleEclipse()
+	{
+		if (Find.World.GameConditionManager.ConditionIsActive(GameConditionDefOf.Eclipse))
+		{
+			Find.World.GameConditionManager.ActiveConditions.Find(x => x.def == GameConditionDefOf.Eclipse).End();
+		}
+		else
+		{
+			IncidentParms incidentParms = new IncidentParms()
+			{
+				forced = true,
+				target = Find.World
+			};
+			DefDatabase<IncidentDef>.GetNamed("Eclipse").Worker.TryExecute(incidentParms);
+		}
 	}
 }
