20fddbc91f22aaf75408ae879e5d8feb4a0b9622
Dexter Lowe <feldoh10@gmail.com>
2023-04-03 01:01:36 +0100

    Slightly more evil

--- 1.4/Source/Meme-Superpack/IncidentWorker_Homeless.cs
+++ 1.4/Source/Meme-Superpack/IncidentWorker_Homeless.cs
@@ -17,7 +17,16 @@ public class IncidentWorker_Homeless : IncidentWorker
 		Map target = (Map)parms.target;
 		var beds = target.listerBuildings.allBuildingsColonist.Where(b => b.def.IsBed && b.FlammableNow).ToList();
 		if (beds.Count <= 0) return false;
-		foreach (Building b in beds) FireUtility.TryStartFireIn(b.Position, target, Rand.Range(0.420f, 0.69f));
+		foreach (Building b in beds)
+		{
+			var radius = Rand.Range(1f, 3f);
+			IntVec3 position = b.Position;
+			if (Rand.Chance(0.69f)) b.Destroy(DestroyMode.KillFinalize);
+			FireBurstUtility.ThrowFuelTick(position, radius, target);
+			GenExplosion.DoExplosion(position, target, radius, DamageDefOf.Flame, null);
+			FireUtility.TryStartFireIn(position, target, Rand.Range(0.420f, 0.69f));
+		}
+
 		if (SendLetter) SendStandardLetter(parms, new LookTargets(beds));
 		return true;
 	}
