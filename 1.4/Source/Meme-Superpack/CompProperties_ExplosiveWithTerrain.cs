3d85ba5c1f5cfd1f566d7c766b2703440efbcb32
Dexter Lowe <feldoh10@gmail.com>
2023-04-15 21:43:45 +0100

    Update CompProperties_ExplosiveWithTerrain.cs

--- 1.4/Source/Meme-Superpack/CompProperties_ExplosiveWithTerrain.cs
+++ 1.4/Source/Meme-Superpack/CompProperties_ExplosiveWithTerrain.cs
@@ -5,8 +5,13 @@ namespace MSS.MemeSuperpack
 {
 	public class CompProperties_ExplosiveWithTerrain : CompProperties_Explosive
 	{
-		public TerrainDef terrain = TerrainDefOf.Concrete;
+		public TerrainDef terrain = null;
 
 		public CompProperties_ExplosiveWithTerrain() => compClass = typeof(Comp_ExplosiveWithTerrain);
+
+		public override void ResolveReferences(ThingDef parentDef)
+		{
+			if (terrain == null) terrain = TerrainDefOf.Concrete;
+		}
 	}
 }
