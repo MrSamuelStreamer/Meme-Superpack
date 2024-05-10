bf38627494fc6c03a79ad677cc933224bf6b6e4d
Dexter Lowe <feldoh10@gmail.com>
2024-05-10 01:23:13 +0100

    #101 fix Waifu present log spam

--- 1.5/Source/Meme-Superpack/Waifu/ThoughtWorker_Precept_Waifu_Present.cs
+++ 1.5/Source/Meme-Superpack/Waifu/ThoughtWorker_Precept_Waifu_Present.cs
@@ -9,7 +9,7 @@ public class ThoughtWorker_Precept_Waifu_Present : ThoughtWorker_Precept
 	protected override ThoughtState ShouldHaveThought(Pawn p) =>
 		!ModsConfig.IdeologyActive || (p.Ideo?.RequiresWaifu() == false || p.Ideo.GetWaifu() == p)
 			? ThoughtState.Inactive
-			: p.MapHeld.mapPawns.AllPawnsSpawned.Any(otherPawn => otherPawn.IsWaifuFor(p))
+			: (p.MapHeld?.mapPawns?.AllPawnsSpawned?.Any(otherPawn => otherPawn.IsWaifuFor(p)) ?? false)
 				? ThoughtState.ActiveAtStage(1)
 				: ThoughtState.ActiveAtStage(0, "No waifus on the map");
 }
